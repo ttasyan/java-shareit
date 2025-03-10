46f2076a538866d9bcd254296904dcfc4e107a16
Avdulova <ttasina00@mail.ru>
2025-03-10 22:04:52 +0300

    правки тестов #4

--- server/src/test/java/ru/practicum/shareit/BookingServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/BookingServiceImplTest.java
@@ -7,12 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.BookingRepository;
-import ru.practicum.shareit.item.ItemService;
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 
-import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -24,22 +21,18 @@ import static org.hamcrest.Matchers.notNullValue;
 @SpringBootTest(classes = ShareItApp.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class BookingServiceImplTest {
     private final BookingRepository repository;
-    private final ItemService itemService;
     private final UserRepository userRepository;
 
 
     @Test
     void saveBooking() {
         long userId = 1L;
-        userRepository.save(new User(userId, "f", "f@mail.ru"));
-        ItemDto itemDto = new ItemDto(1, "Пётр", "ygi", true,
-                null, null, userId, new ArrayList<>(), null);
-        itemService.addItem(userId, itemDto);
-        Booking booking1 = new Booking(1, null, null, null, null, Booking.StatusType.WAITING);
+        User user = userRepository.save(new User(userId, "f", "f@mail.ru"));
+        Booking booking1 = new Booking(1, null, null, null, user, Booking.StatusType.WAITING);
         Booking booking2 = repository.save(booking1);
 
         assertThat(booking2.getId(), notNullValue());
-        assertThat(booking2.getItem().getId(), equalTo(itemDto.getId()));
+        assertThat(booking2.getBooker().getId(), equalTo(userId));
         assertThat(booking2.getStatus(), equalTo("WAITING"));
 
     }
